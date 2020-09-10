dd8c6fd132c7de170320cd0a9fc27d57a7567d71
sagar6462 <sagaarpkr@gmail.com>
2020-09-10 16:27:37 +0545

    Day 1 Code of amazon clone build

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCC_qJiKybjwMUp6aMYD-uvqnc0uY-E_wY",
+	authDomain: "clone-build.firebaseapp.com",
+	databaseURL: "https://clone-build.firebaseio.com",
+	projectId: "clone-build",
+	storageBucket: "clone-build.appspot.com",
+	messagingSenderId: "746065498966",
+	appId: "1:746065498966:web:eda2e61f01f9a2ccd27cf7",
+	measurementId: "G-6343RK0DKK",
+};
