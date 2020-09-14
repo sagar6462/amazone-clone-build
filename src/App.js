b2636112b308e9dc14c9a46612427317d479720a
sagar6462 <sagaarpkr@gmail.com>
2020-09-14 17:10:24 +0545

    End of Day 3 Challenge.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
 import Header from "./Header";
@@ -6,27 +6,50 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-	return (
-		<Router>
-			<div className='app'>
-				<Switch>
-					<Route path='/login'>
-						<Login />
-					</Route>
-					<Route path='/checkout'>
-						<Header />
-						<Checkout />
-					</Route>
-					<Route path='/'>
-						<Header />
-						<Home />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+  const [{ user }, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app loads
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The User is >>>", authUser);
+
+      if (authUser) {
+        // user is logged in / was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
